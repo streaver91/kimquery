9c2141dc90aeab9bc41712600ebf5a84ffd2d2c0
Junhao Li <streaver91@gmail.com>
2016-02-27 18:08:49 -0500

    add use strict and some doc to kimquery; roughly complete controller

--- bin/kimquery.js
+++ bin/kimquery.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+/*!
+ * kimquery - CLI for retrieving data from OpenKIM 
+ */
 
+'use strict';
+
+// Use controller to handle command line inputs
 var controller = require('../lib/controller.js');
 var argv = require('minimist')(process.argv.slice(2));
 controller.process(argv);
